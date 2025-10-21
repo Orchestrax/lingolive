719464a6b4001ebaf3591d6aef2fa003c8bc544b
chandermanimishra91@gmail.com <chandermanimishra91@gmail.com>
2025-10-21 21:10:40 +0530

    last phase 9th

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     if (loading) <Loading />;
 
   return (
-      <div className="bg-gradient-to-br from-gray-900 via-gray-950 to-black min-h-screen text-gray-100">
+      <div className="bg-black min-h-screen text-gray-100">
         <Navbar />
         <Suspense fallback={<Loading />}>
           <Routes>
